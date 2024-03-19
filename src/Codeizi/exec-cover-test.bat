3629f898733a9e1d16f5981f21b0449bfe9eb577
Douglas Mendes <douglasaleixomendes@gmail.com>
2024-03-18 23:00:32 -0300

    Update exec-cover-test.bat

--- src/Codeizi/exec-cover-test.bat
+++ src/Codeizi/exec-cover-test.bat
@@ -2,4 +2,5 @@ rmdir /s /q coverage
 rmdir /s /q CoverageReport
 dotnet test --collect:"XPlat Code Coverage;Format=lcov" --results-directory:"./coverage" 
 reportgenerator -reports:./coverage/*/*coverage.info -targetdir:./CoverageReport -reporttypes:HTML
-pause
+rmdir /s /q coverage
+start ./CoverageReport/index.html
